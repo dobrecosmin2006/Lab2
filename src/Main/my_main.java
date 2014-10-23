5489b79a1d958737aa7928d5738e87ddee208b96
CosminDobre <dobrecosmin2006@gmail.com>
2014-10-23 05:46:44 +0300

    first

---
+++ src/Main/my_main.java
@@ -0,0 +1,14 @@
+package Main;
+
+/**
+ * Created by Mihai on 10/23/2014.
+ */
+public class my_main {
+
+
+    public static void main ( String[]arguments)
+    {
+        System.out.println("Hello Lab2 ");
+    }
+
+}
